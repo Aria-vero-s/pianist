4c90177935da30e6ef486631465e5b1fee8d3523
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2023-10-24 18:29:34 +0100

    added new photos

--- script.js
+++ script.js
@@ -474,3 +474,23 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 >>>>>>> aaa46d3 (initial commit)
 });
+
+const audio = document.getElementById('audio');
+const playButton = document.getElementById('play-button');
+const waveform = document.getElementById('waveform');
+const soundcloudLink = document.getElementById('soundcloud-link');
+
+// Add an event listener to the play button
+playButton.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        playButton.textContent = 'Pause';
+    } else {
+        audio.pause();
+        playButton.textContent = 'Play';
+    }
+});
+
+// You can use additional libraries to visualize the waveform.
+
+// You can embed a SoundCloud link as shown in the HTML.
