a421760c7e8ee4607a0fc491a4590147715ef285
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2023-10-27 20:53:41 +0100

    new navbar style

--- code.js
+++ code.js
@@ -1,30 +1,15 @@
 //Function for mobile navbar
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Get references to the menu icon and navbar
+    // Get references to the elements
     const menuIcon = document.getElementById("menu-icon");
     const navbar = document.getElementById("navbar");
 
-    // Add a click event listener to toggle the menu
+    // Add a click event listener to the menu icon
     menuIcon.addEventListener("click", function () {
     navbar.classList.toggle("show-mobile-links");
-    logo.classList.toggle("hide-logo");
-    toggleMenuIcon();
     });
 
-    // Function to toggle the menu icon
-    function toggleMenuIcon() {
-        const icon = menuIcon.querySelector("i");
-        if (navbar.classList.contains("show-mobile-links")) {
-            // Change the menu icon to an X
-            icon.classList.remove("fa-bars");
-            icon.classList.add("fa-times");
-        } else {
-            // Change the X back to the menu icon
-            icon.classList.remove("fa-times");
-            icon.classList.add("fa-bars");
-        }
-    }
 });
 
 
@@ -35,17 +20,17 @@ document.addEventListener('DOMContentLoaded', function() {
     english: {
       about: {
         h1: "Hello, I'm Paolo",
-        p: "I started playing the piano in Naples when I was 9, and I perfected my technique through classical music studies in Italy, before moving to France to study modern harmony and jazz. In Paris I completed a master’s degree in music and philosophy at the École Normale Supérieure de Paris, with a dissertation on the aesthetics of music and French structuralism. In the meantime, I focused my playing on jazz and started my career as a professional pianist amidst the intoxicating rhythms of the City of Light, where I performed regularly at the heart of Montmartre’s legendary cabarets and animated its intimate cafes nestled within the Latin Quarter. Embracing a harmonious blend of musical traditions, my style evolved to be a unique fusion of Italian and French music legacies, interwoven with a deep appreciation for the rhythmic richness of African-American and Latin-American music. This international approach to music performance, combined with a deep-seated interest in ethnomusicology, eventually led me to direct the first World Music workshop at the renowned Cité Internationale Universitaire de Paris, Europe’s largest international student campus, where my diverse influences culminated into a celebration of global musical diversity."
+        p: "I started playing the piano in Naples when I was 9, and I perfected my technique through classical music studies in Italy, before moving to France to study modern harmony and jazz. In Paris I completed a master’s degree in music and philosophy at the École Normale Supérieure de Paris, with a dissertation on the aesthetics of music and French structuralism. In the meantime, I focused my playing on jazz and started my career as a professional pianist in Parisian venues. Embracing a harmonious blend of musical traditions, my style evolved to be a unique fusion of Southern Italian and French music legacies, interwoven with a deep appreciation for the rhythmic richness of Brazilian music. This international approach to music eventually led me to direct the first World Music workshop at the renowned Cité Internationale Universitaire de Paris, Europe’s largest international student campus, where my diverse influences culminated into a celebration of global musical diversity."
       },
       lessons: {
         h1: "Lessons",
         p: "I started playing the piano in Naples when I was 9, and I perfected my technique through classical music studies in Italy, before moving to France to study modern harmony and jazz. In Paris I completed a master’s degree in music and philosophy at the École Normale Supérieure de Paris, with a dissertation on the aesthetics of music and French structuralism. In the meantime, I focused my playing on jazz and started my career as a professional pianist in Parisian venues."
       },
       navbar: {
-        link1: "Home",
-        link2: "About",
-        link3: "Lessons",
-        link4: "Contact",
+        home: "Home",
+        about: "About",
+        lessons: "Lessons",
+        contact: "Contact"
       },
       contact: {
         labelName: "Name:",
@@ -65,10 +50,10 @@ document.addEventListener('DOMContentLoaded', function() {
         p: "J'ai commencé le piano à Naples à l'âge de 9 ans et j'ai perfectionné ma technique en étudiant la musique classique en Italie, avant de m'installer en France pour étudier l'harmonie moderne et le jazz. À Paris, j’ai complété une maîtrise de musique et de philosophie à l’École Normale Supérieure de Paris, avec un mémoire sur l’esthétique de la musique et le structuralisme français. Parallèlement, j'oriente mon jeu vers le jazz et débute ma carrière de pianiste professionnel dans les salles parisiennes. Embrassant un mélange harmonieux de traditions musicales, mon style a évolué pour devenir une fusion unique des héritages musicaux du sud de l'Italie et de la France, entrelacés avec une profonde appréciation de la richesse rythmique de la musique brésilienne. Cette approche internationale de la musique m’a finalement amené à diriger le premier atelier de musique du monde à la célèbre Cité Internationale Universitaire de Paris, le plus grand campus étudiant international d’Europe, où mes diverses influences ont abouti à une célébration de la diversité musicale mondiale."
       },
       navbar: {
-        link1: "Accueil",
-        link2: "À propos",
-        link3: "Cours",
-        link4: "Contact",
+        home: "Accueil",
+        about: "Bio",
+        lessons: "Leçons",
+        contact: "Contact"
       },
       contact: {
         labelName: "Nom:",
@@ -88,10 +73,10 @@ document.addEventListener('DOMContentLoaded', function() {
         p: "Ho iniziato a suonare il pianoforte a Napoli all'età di 9 anni, e ho perfezionato la mia tecnica attraverso gli studi di musica classica in Italia, prima di trasferirmi in Francia per studiare armonia moderna e jazz. A Parigi ho conseguito un master in musica e filosofia presso l’École Normale Supérieure de Paris, con una tesi sull’estetica della musica e sullo strutturalismo francese. Nel frattempo mi sono concentrato sul jazz e ho iniziato la mia carriera come pianista professionista nei locali parigini. Abbracciando un'armoniosa miscela di tradizioni musicali, il mio stile si è evoluto fino a diventare una fusione unica delle eredità musicali dell'Italia meridionale e della Francia, intrecciate con un profondo apprezzamento per la ricchezza ritmica della musica brasiliana. Questo approccio internazionale alla musica mi ha portato alla fine a dirigere il primo workshop di World Music presso la rinomata Cité Internationale Universitaire de Paris, il più grande campus studentesco internazionale d’Europa, dove le mie diverse influenze sono culminate in una celebrazione della diversità musicale globale."
       },
       navbar: {
-        link1: "Home",
-        link2: "Chi Siamo",
-        link3: "Lezioni",
-        link4: "Contatti",
+        home: "Home",
+        about: "Bio",
+        lessons: "Lezioni",
+        contact: "Contatti"
       },
       contact: {
         labelName: "Nome:",
@@ -127,16 +112,16 @@ document.addEventListener('DOMContentLoaded', function() {
     p.textContent = sectionData.p;
   }
 
-    // Function to update navbar content
-    function updateNavbarContent(navbarData) {
-        const navbarLinks = document.querySelectorAll('.navbar-links-left li a, .navbar-links-right li a');
-    
-        navbarLinks[0].textContent = navbarData.link1; // Update link 1
-        navbarLinks[1].textContent = navbarData.link2; // Update link 2
-        navbarLinks[2].textContent = navbarData.link3; // Update link 3
-        navbarLinks[3].textContent = navbarData.link4; // Update link 4
-    }
-    
+  // Function to update navbar content
+  function updateNavbarContent(navbarData) {
+    const navbar = document.getElementById('navbar');
+    const navbarLinks = navbar.querySelectorAll('ul.navbar-links li a');
+
+    navbarLinks[0].textContent = navbarData.home;
+    navbarLinks[1].textContent = navbarData.about;
+    navbarLinks[2].textContent = navbarData.lessons;
+    navbarLinks[3].textContent = navbarData.contact;
+  }
 
   // Function to update contact form content
   function updateContactFormContent(contactData) {
