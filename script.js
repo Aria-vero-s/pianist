6d337541f6eef2dba2a35c52712cbaecb580bdbb
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2023-10-29 15:28:05 +0000

    updated gitignore and readme file

--- script.js
+++ script.js
@@ -516,6 +516,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 // // Email configuration
 
 // document.addEventListener('DOMContentLoaded', function() {
@@ -681,19 +682,23 @@ playButton.addEventListener('click', () => {
 // You can embed a SoundCloud link as shown in the HTML.
 =======
 // Email configuration
+=======
+// // Email configuration
+>>>>>>> f28ec63 (updated gitignore and readme file)
 
-document.addEventListener('DOMContentLoaded', function() {
-  emailjs.init("itnHyNd7JvizqyR1r");
+// document.addEventListener('DOMContentLoaded', function() {
+//   emailjs.init("public key");
 
-  console.log("DOMContentLoaded event fired");
-  const contactForm = document.getElementById("contact-form");
-  const successMessage = document.getElementById("success-message");
-  const errorMessage = document.getElementById("error-message");
+//   console.log("DOMContentLoaded event fired");
+//   const contactForm = document.getElementById("contact-form");
+//   const successMessage = document.getElementById("success-message");
+//   const errorMessage = document.getElementById("error-message");
 
-  // Add a submit event listener to the form
-  contactForm.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the default form submission
+//   // Add a submit event listener to the form
+//   contactForm.addEventListener("submit", function(event) {
+//     event.preventDefault(); // Prevent the default form submission
 
+<<<<<<< HEAD
 <<<<<<< HEAD
   // Send the email
   emailjs.send("service_djf8d02", "template_id", {
@@ -718,21 +723,28 @@ document.addEventListener('DOMContentLoaded', function() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
+=======
+//       // Get form data
+//     const name = document.getElementById("name").value;
+//     const email = document.getElementById("email").value;
+//     const message = document.getElementById("message").value;
+>>>>>>> f28ec63 (updated gitignore and readme file)
 
-    console.log("Form submitted");
+//     console.log("Form submitted");
 
-    // Use the emailjs.sendForm method to send the form
-    emailjs
-      .sendForm("service_cm2dnwk", "template_s9h32mu", contactForm)
-      .then(
-        function(response) {
-          console.log("Email sent successfully", response);
-          successMessage.style.display = "block";
-        },
-        function(error) {
-          console.log("Email sending failed", error);
-          console.error(error);
+//     // Use the emailjs.sendForm method to send the form
+//     emailjs
+//       .sendForm("service_id", "template_id", contactForm)
+//       .then(
+//         function(response) {
+//           console.log("Email sent successfully", response);
+//           successMessage.style.display = "block";
+//         },
+//         function(error) {
+//           console.log("Email sending failed", error);
+//           console.error(error);
 
+<<<<<<< HEAD
           // Additional debugging information:
           if (error.response) {
             console.log("Response data:", error.response.data);
@@ -745,3 +757,16 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 >>>>>>> 378b1e0 (email configuration complete and testing done)
+=======
+//           // Additional debugging information:
+//           if (error.response) {
+//             console.log("Response data:", error.response.data);
+//             console.log("HTTP status:", error.response.status);
+//             console.log("Headers:", error.response.headers);
+//           }
+//           errorMessage.style.display = "block";
+//         }
+//       );
+//   });
+// });
+>>>>>>> f28ec63 (updated gitignore and readme file)
